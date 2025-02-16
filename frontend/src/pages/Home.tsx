313c4e2c4ca111e687cf333121ad6c3e0fb398e7
marymo <maostar1010@gmail.com>
2025-02-15 20:48:50 -0700

    front-end spacing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="flex flex-col flex-grow items-center justify-center space-y-8 text-text">
-      <div className="flex items-center justify-center space-x-10">
+      <div className="flex items-center justify-center space-x-10 mt-20">
         <img src={logo} alt="StockEd Logo" className="w-1/4" />
         <div className="flex flex-col space-y-2 w-1/3">
-          <p className="text-3xl text-center font-extrabold">
+          <p className="text-3xl text-center font-extrabold mt-10 mb-5">
             Learn and practice stock trading in a fun, gamified environment.
+          </p>
+          <p className="text-3xl text-center font-extrabold">
             Start your journey to becoming a stock market pro!
           </p>
           <div className="flex flex-row space-x-4 justify-center">
-            <Link to="/account" className="btn btn-primary transition-colors">
+            <Link to="/account" className="btn btn-primary transition-colors mt-8">
               Account
             </Link>
             <Link
               to="/leaderboard"
-              className="btn btn-primary transition-colors"
+              className="btn btn-primary transition-colors mt-8"
             >
               Leaderboard
             </Link>
