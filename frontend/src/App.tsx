1503c31b9fbeb509b293889ac8e8c84a4da0eda5
j444zhan <j444zhan@uwaterloo.ca>
2025-02-15 19:13:01 -0700

    added footer animation and changed some styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Market from "./pages/Market";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="flex min-h-screen">
         <Navbar />
-        <div className="flex-1 flex flex-col">
-          <main className="flex-grow container mx-auto px-4 py-8">
+        <div className="flex-1 flex flex-col flex-grow">
+          <main className="flex-grow px-4 py-8">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/market" element={<Market />} />
             </Routes>
           </main>
-          <footer className="bg-accent text-white p-4 text-center">
-            get stock info
-          </footer>
+          <Footer />
         </div>
       </div>
     </Router>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,27 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-8">
-      <img src={logo} alt="StockEd Logo" className="w-1/2" />
-      <h1 className="text-4xl font-bold text-center">StockEd</h1>
-      <p className="text-xl text-center max-w-2xl">
-        Learn and practice stock trading in a fun, gamified environment. Start
-        your journey to becoming a stock market pro!
-      </p>
-      <div className="card p-8 text-center ">
-        <h2 className="text-2xl font-semibold mb-4">Get Started</h2>
-        <div className="flex flex-row space-x-4 justify-center">
-          <Link 
-            to="/account" 
-            className="btn btn-primary transition-colors"
-          >
-            Account
-          </Link>
-          <Link 
-            to="/leaderboard" 
-            className="btn btn-primary transition-colors">
-            Leaderboard
-          </Link>
+    <div className="flex flex-col flex-grow items-center justify-center space-y-8 text-text">
+      <div className="flex items-center justify-center space-x-10">
+        <img src={logo} alt="StockEd Logo" className="w-1/4" />
+        <div className="flex flex-col space-y-2 w-1/3">
+          <p className="text-3xl text-center font-extrabold">
+            Learn and practice stock trading in a fun, gamified environment.
+            Start your journey to becoming a stock market pro!
+          </p>
+          <div className="flex flex-row space-x-4 justify-center">
+            <Link to="/account" className="btn btn-primary transition-colors">
+              Account
+            </Link>
+            <Link
+              to="/leaderboard"
+              className="btn btn-primary transition-colors"
+            >
+              Leaderboard
+            </Link>
+          </div>
         </div>
       </div>
     </div>
